b4adf2e2f50ff8ce266f355e8c289abde471e09f
vishnusankar55 <vishnusankar55@gmail.com>
2024-07-03 09:16:22 +0530

    changes updated

---
+++ social_network/users/models.py
@@ -0,0 +1,13 @@
+# users/models.py
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
