fe5484f8ca272d12b23a9b9e21620e103050722a
vishnusankar55 <vishnusankar55@gmail.com>
2024-07-03 09:20:13 +0530

    changes updated in admin.py

--- social_network/users/admin.py
+++ social_network/users/admin.py
@@ -1,3 +1,21 @@
-from django.contrib import admin
+# users/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['email', 'username', 'is_staff', 'is_active']
+    list_filter = ['email', 'username', 'is_staff', 'is_active']
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'username')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {'fields': ('email', 'password1', 'password2', 'username')}),
+    )
+    search_fields = ['email', 'username']
+    ordering = ['email']
+
+admin.site.register(CustomUser, CustomUserAdmin)
