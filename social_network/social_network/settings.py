57aec3bd78d19d5a2a85075aefd41c0306f589ef
vishnusankar55 <vishnusankar55@gmail.com>
2024-07-03 09:33:47 +0530

    changes updated in serializers.py

--- social_network/social_network/settings.py
+++ social_network/social_network/settings.py
@@ -45,10 +45,17 @@ INSTALLED_APPS = [
 
 AUTH_USER_MODEL = 'users.User'
 
+# socialnetwork/settings.py
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 MIDDLEWARE = [
---
+++ social_network/users/serializers.py
@@ -0,0 +1,24 @@
+# users/serializers.py
+
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            password=validated_data['password']
+        )
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
